c4741b82609fd7ab7561f73108ac163175ee377a
nyr1k <zolamanuraly@gmail.com>
2026-05-13 11:02:51 +0500

    testing

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,12 +1,15 @@
 #include <kernel/tty.h>
+#include <stdio.h>
+#include <string.h>
 
 void kernel_main(void) 
 {
   /* Initialize terminal interface */
   terminal_initialize();
-  
-  for(int i = 0; i < 15; i++) 
-    terminal_write_string("Hello, bro teams!\n");
+  printf("Hello World from brOS\n");
+  char buf[6]; 
+  memset(buf, '0', 6);
+  printf("%s\n", buf);
 }
 
 
