6918873e781837c49460104050344d67faa82b5e
nyr1k <zolamanuraly@gmail.com>
2026-05-13 11:01:55 +0500

    added memset, memcpy

--- libc/include/string.h
+++ libc/include/string.h
@@ -6,10 +6,15 @@
 #include <stddef.h>
 
 #ifdef __cplusplus
+#define _Restrict
 extern "C" {
+#else 
+#define _Restrict restrict 
 #endif
 
 size_t strlen(const char* str);
+void* memset(void* ptr, int value, size_t num);
+void* memcpy(void* _Restrict destination, const void* _Restrict source, size_t num);
 
 #ifdef __cplusplus
 }
