f9c38a0bb386e87b21d3c034b7786047f0f1084c
nyr1k <zolamanuraly@gmail.com>
2026-05-16 00:19:37 +0500

    small fixes

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,15 +1,23 @@
 #include <kernel/tty.h>
+#include <arch/i386/gdt.h>
 #include <stdio.h>
 #include <string.h>
+
+
+#include <stdint.h>
 
 void kernel_main(void) 
 {
   /* Initialize terminal interface */
   terminal_initialize();
+ 
+  gdt_init(); 
+  printf("GDT is set\n");
+
   printf("Hello World from brOS\n");
-  char buf[6]; 
-  memset(buf, '0', 6);
-  printf("%s\n", buf);
+  
+  printf("erasik karasyk\n");
+  printf("arslanini bituzini\n");
 }
 
 
