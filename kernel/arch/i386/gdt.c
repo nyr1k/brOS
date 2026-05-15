ca85144d7c062c6b98bf24e4327070f1abf0a7ea
nyr1k <zolamanuraly@gmail.com>
2026-05-16 00:18:24 +0500

    finish init

--- kernel/arch/i386/gdt.c
+++ kernel/arch/i386/gdt.c
@@ -9,10 +9,17 @@ static uint64_t gdt_create_descriptor(uint32_t base, uint32_t limit, uint8_t acc
   descriptor |= (uint64_t)((base >> 16) & 0xFF) << 32;
   descriptor |= (uint64_t)access << 40;
   descriptor |= (uint64_t)((limit >> 16) & 0x0F) << 48; 
-  descriptor |= (uint64_t)(flags & 0xF0) << 48;
+  descriptor |= (uint64_t)(flags & 0x0F) << 52;
   descriptor |= (uint64_t)((base >> 24) & 0xFF) << 56;
   return descriptor; 
 }
+
+struct __attribute__((packed)) gdt_desc {
+  uint16_t size; 
+  uint32_t start; 
+};
+
+extern void load_gdt(struct gdt_desc*); 
 
 /* 
  *  8 bytes null descriptor 
@@ -21,17 +28,15 @@ static uint64_t gdt_create_descriptor(uint32_t base, uint32_t limit, uint8_t acc
  *  TODO ring 1-3 
  * */
 static uint64_t gdt[3];
+static struct gdt_desc gdtr; 
 
 void gdt_init(void) 
 {
   gdt[0] = 0;   
+  gdt[1] = gdt_create_descriptor(0x00000000, 0x000FFFFF, 0x9B, 0x0C); // kernel code segment
+  gdt[2] = gdt_create_descriptor(0x00000000, 0x000FFFFF, 0x93, 0x0C); // kernel data segment
+  gdtr.size = sizeof(gdt) - 1;
+  gdtr.start = (uint32_t)&gdt; 
 
-  /* create kernel descriptor */;
-  uint8_t code_access = 0x9B; // 1001 1011  
-  uint8_t code_flags = 0xC0; // 1100 0000
-  uint8_t data_access = 0x93; // 1001 0011
-  uint8_t data_flags = 0xC0; // 1100 0000
-
-  gdt[1] = gdt_create_descriptor(0x00000000, 0x000FFFFF, code_access, code_flags);
-  gdt[2] = gdt_create_descriptor(0x00000000, 0x000FFFFF, data_access, data_flags);
+  load_gdt(&gdtr); 
 }
