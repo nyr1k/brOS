6fa317ff7987391c0e2bf8baf6904e68f5a418e9
nyr1k <zolamanuraly@gmail.com>
2026-05-19 18:17:59 +0500

    Create memory and interrupts directories. Move GDT related code to the memory folder, and IDT related code to the interrupts folder

---
+++ kernel/arch/i386/interrupts/idt.c
@@ -0,0 +1,125 @@
+#include <arch/i386/idt.h> 
+#include <stdint.h> 
+#include <string.h>
+
+
+/**
+ * 0-31 gates are reserved by the CPU
+ */
+extern void isr0(void);
+extern void isr1(void);
+extern void isr2(void);
+extern void isr3(void);
+extern void isr4(void);
+extern void isr5(void);
+extern void isr6(void);
+extern void isr7(void);
+extern void isr8(void);
+extern void isr9(void);
+extern void isr10(void);
+extern void isr11(void);
+extern void isr12(void);
+extern void isr13(void);
+extern void isr14(void);
+extern void isr15(void);
+extern void isr16(void);  
+extern void isr17(void);
+extern void isr18(void);
+extern void isr19(void);
+extern void isr20(void);
+extern void isr21(void);
+extern void isr22(void);
+extern void isr23(void);
+extern void isr24(void);
+extern void isr25(void);
+extern void isr26(void);
+extern void isr27(void);
+extern void isr28(void);
+extern void isr29(void);
+extern void isr30(void);
+extern void isr31(void);
+
+typedef struct __attribute__((packed)) idt_entry {
+  uint16_t offset_low;
+  uint16_t selector;
+  uint8_t zero;
+  uint8_t attributes;
+  uint16_t offset_high; 
+} idt_entry_t;
+
+typedef struct __attribute__((packed)) idt_ptr {
+  uint16_t size;
+  uint32_t offset; 
+} idt_ptr_t;
+
+static idt_entry_t idt[256];
+static idt_ptr_t idtr;
+
+void set_idt_gate(uint8_t index, uint32_t offset, uint16_t selector, uint8_t attributes)  
+{
+  idt[index].offset_low = offset & 0xFFFF;
+  idt[index].selector = selector;
+  idt[index].zero = 0;
+  idt[index].attributes = attributes;
+  idt[index].offset_high = (offset >> 16) & 0xFFFF;
+}
+
+extern void load_idt(idt_ptr_t*);
+
+void idt_init(void) 
+{
+  /* Initialize all elements to zero */
+  memset((void*) idt, 0, sizeof(idt));
+  /**
+   * the selector must point to a valid code segment 
+   *  in our case it is 0x08
+   * 
+   * the attributes consist of the following:
+   *  1. present bit - set to one to make the descriptor valid
+   *  2. DPL (2 bits) - 0 (Kernel mode) 
+   *  3. zero bit 
+   *  4. gate type (4 bits) - 0xE if it's an interrupt gate
+   *    and 0xF if it's a trap gate 
+   * 
+   *  So, for the interrupt gates attributes = 0x8E
+   *    for the trap gates attributes = 0x8F
+   */
+  set_idt_gate(0, (uint32_t) isr0, 0x08, 0x8E);
+  set_idt_gate(1, (uint32_t) isr1, 0x08, 0x8E);
+  set_idt_gate(2, (uint32_t) isr2, 0x08, 0x8E);
+  set_idt_gate(3, (uint32_t) isr3, 0x08, 0x8E);
+  set_idt_gate(4, (uint32_t) isr4, 0x08, 0x8E);
+  set_idt_gate(5, (uint32_t) isr5, 0x08, 0x8E);
+  set_idt_gate(6, (uint32_t) isr6, 0x08, 0x8E);
+  set_idt_gate(7, (uint32_t) isr7, 0x08, 0x8E);
+  set_idt_gate(8, (uint32_t) isr8, 0x08, 0x8E);
+  set_idt_gate(9, (uint32_t) isr9, 0x08, 0x8E);
+  set_idt_gate(10, (uint32_t) isr10, 0x08, 0x8E);
+  set_idt_gate(11, (uint32_t) isr11, 0x08, 0x8E);
+  set_idt_gate(12, (uint32_t) isr12, 0x08, 0x8E);
+  set_idt_gate(13, (uint32_t) isr13, 0x08, 0x8E);
+  set_idt_gate(14, (uint32_t) isr14, 0x08, 0x8E);
+  set_idt_gate(15, (uint32_t) isr15, 0x08, 0x8E);
+  set_idt_gate(16, (uint32_t) isr16, 0x08, 0x8E);
+  set_idt_gate(17, (uint32_t) isr17, 0x08, 0x8E);
+  set_idt_gate(18, (uint32_t) isr18, 0x08, 0x8E);
+  set_idt_gate(19, (uint32_t) isr19, 0x08, 0x8E);
+  set_idt_gate(20, (uint32_t) isr20, 0x08, 0x8E);
+  set_idt_gate(21, (uint32_t) isr21, 0x08, 0x8E);
+  set_idt_gate(22, (uint32_t) isr22, 0x08, 0x8E);
+  set_idt_gate(23, (uint32_t) isr23, 0x08, 0x8E);
+  set_idt_gate(24, (uint32_t) isr24, 0x08, 0x8E);
+  set_idt_gate(25, (uint32_t) isr25, 0x08, 0x8E);
+  set_idt_gate(26, (uint32_t) isr26, 0x08, 0x8E);
+  set_idt_gate(27, (uint32_t) isr27, 0x08, 0x8E);
+  set_idt_gate(28, (uint32_t) isr28, 0x08, 0x8E);
+  set_idt_gate(29, (uint32_t) isr29, 0x08, 0x8E);
+  set_idt_gate(30, (uint32_t) isr30, 0x08, 0x8E);
+  set_idt_gate(31, (uint32_t) isr31, 0x08, 0x8E);
+
+
+  /* Pass the information about the IDT to the IDT register on the CPU */
+  idtr.size = sizeof(idt) - 1;
+  idtr.offset = (uint32_t)idt;
+  load_idt(&idtr);
+}
