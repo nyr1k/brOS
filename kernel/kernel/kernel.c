67a44480d4362ec9d0046909f5c28a41b8af957a
nyr1k <zolamanuraly@gmail.com>
2026-05-19 18:23:16 +0500

    check if IDT is working

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,5 +1,6 @@
 #include <kernel/tty.h>
 #include <arch/i386/gdt.h>
+#include <arch/i386/idt.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -12,12 +13,19 @@ void kernel_main(void)
   terminal_initialize();
  
   gdt_init(); 
-  printf("GDT is set\n");
+  printf("GDT is set\n"); 
+  idt_init();
+  printf("IDT is set\n");
 
   printf("Hello World from brOS\n");
-  
-  printf("erasik karasyk\n");
-  printf("arslanini bituzini\n");
+    
+  /* Check if the IDT works */ 
+  for (int i = 0; i < 10; i++) {
+    if (i == 5) {
+      printf("%d", i/0);
+    }
+    printf("%d\n", i);
+  }
 }
 
 
